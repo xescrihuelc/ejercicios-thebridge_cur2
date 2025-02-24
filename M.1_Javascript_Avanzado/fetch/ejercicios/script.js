90cd936ed7184f9bd03d566aa31de951729b2028
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-02-24 13:21:57 +0100

    Ejercicio resuleto

--- M.1_Javascript_Avanzado/fetch/ejercicios/script.js
+++ M.1_Javascript_Avanzado/fetch/ejercicios/script.js
@@ -1,1 +1,26 @@
 //Aquí tu código
+const showJoke = () => {
+    fetch("https://v2.jokeapi.dev/joke/Programming?lang=es")
+        .then((response) => response.json())
+        .then((data) => {
+            // Manipulación de las datos recuperados
+            const joke = document.getElementById("chiste");
+            if (data.joke === undefined) {
+                joke.innerHTML = `
+                ${data.setup}<br/>${data.delivery}
+                `;
+            } else {
+                joke.innerText = `
+                ${data.joke}
+                `;
+            }
+        })
+        .catch((error) => {
+            //Manejo de errores en el solicitud
+            console.error("Error en la solicitud:", error);
+        });
+};
+
+/* TRIGGERS */
+const p = document.getElementById("obtenerChiste");
+p.addEventListener("click", showJoke);
