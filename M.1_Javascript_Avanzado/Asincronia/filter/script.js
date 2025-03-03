b30db88dac3f466f61b2069c82060dd5a6452c0a
xescrihuelc <xescrihuelc@outlook.com>
2025-03-03 23:11:33 +0100

    Lista filtros añadida, botón busqueda eliminado

--- M.1_Javascript_Avanzado/Asincronia/filter/script.js
+++ M.1_Javascript_Avanzado/Asincronia/filter/script.js
@@ -51,42 +51,64 @@ function busqueda() {
 
 function getResponseAPI(p) {
     let url = "";
-    if (url === "") {
-        url = "https://dragonball-api.com/api/characters?page=1&limit=10";
-    } else if (p == "prev") {
-        url = prevPageLink;
-    } else if (p == "next") {
-        url = nextPageLink;
-    } else {
+    if (p !== "" && p !== "prev" && p !== "next") {
         console.error("Parametro no válido pasado");
         alert("Parametro no válido pasado");
         return;
-    }
+    };
+
+    if (url === "") {
+        url = "https://dragonball-api.com/api/characters?page=1&limit=10";
+    };
+    if (p == "prev") {
+        url = prevPageLink;
+    };
+    if (p == "next") {
+        url = nextPageLink;
+    };
 
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
+            // Ocultar controles
+            searchBar.hidden = true;
+            prevBttn.hidden = true;
+            nxtBttn.hidden = true;
+
+            // Mostrar página de carga
             document.getElementById(
                 "characters"
             ).innerHTML = `<p id="loading">Preparando página...</p>`;
+
+            // Variables de URLs
             prevPageLink = data.links.previous;
             nextPageLink = data.links.next;
+
+            // 
             setTimeout(() => {
                 document.getElementById("characters").innerHTML = "";
                 const characters = getCharacters(data.items);
                 insertCharacters(characters);
-                subSection.style.display = "";
+                filterBox.style.display = "";
+
+                // Condiciones que muestran/ocultan botones de prevPage o nextPage en
+                // función de si exísten URLs o no.
                 if (prevPageLink === "") {
                     prevBttn.disabled = true;
                 } else {
                     prevBttn.disabled = false;
                 }
-
+                
                 if (nextPageLink === "") {
                     nxtBttn.disabled = true;
                 } else {
                     nxtBttn.disabled = false;
                 }
+
+                // Mostrar botones de control
+                prevBttn.hidden = false;
+                nxtBttn.hidden = false;
+                searchBar.hidden = false;
             }, 5000);
         })
         .catch((error) => {
@@ -94,14 +116,16 @@ function getResponseAPI(p) {
         });
 }
 
-const subSection = document.getElementById("searchSection");
-subSection.style.display = "none";
+const filterBox = document.getElementById("filterBox");
+filterBox.style.display = "none";
 getResponseAPI("");
 
 //Triggers
 const searchBar = document.getElementById("searchBar");
 const prevBttn = document.getElementById("prevPage");
 const nxtBttn = document.getElementById("nextPage");
-searchBar.addEventListener("change", busqueda);
+
+// EventListeners
+searchBar.addEventListener("change", () => getResponseAPI());
 prevBttn.addEventListener("click", () => getResponseAPI("prev"));
 nxtBttn.addEventListener("click", () => getResponseAPI("next"));
