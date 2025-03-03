b541c98f122a3c774b29166f82e69940450ec7ea
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-03-03 13:38:13 +0100

    Solucion a nuevo código a corregir

--- M.1_Javascript_Avanzado/Asincronia/filter/script.js
+++ M.1_Javascript_Avanzado/Asincronia/filter/script.js
@@ -90,7 +90,7 @@ const nxtPage = () => {
         .then((response) => response.json())
         .then((data) => {
             document.getElementById(
-                "characters"
+                "characters-"
             ).innerHTML = `<p id="loading">Preparando página...</p>`;
             prevPageLink = data.links.previous;
             nextPageLink = data.links.next;
@@ -158,3 +158,145 @@ const prevBttn = document.getElementById("prevPage");
 prevBttn.addEventListener("click", prevPage);
 const nxtBttn = document.getElementById("nextPage");
 nxtBttn.addEventListener("click", nxtPage);
+
+// Mirar solucion a nuevo codigo
+/* 
+// Busqueda
+let prevPageLink = "";
+let nextPageLink = "";
+
+function lowing(txt) {
+    return String(txt).toLowerCase();
+}
+
+const getCharacters = (p) => {
+    const array = new Map();
+    p.forEach((i) => {
+        array.set(i.id, { name: i.name, image: i.image, desc: i.description });
+    });
+    return array;
+};
+
+const insertCharacters = (p) => {
+    p.forEach((i) => {
+        const list = document.getElementById("characters");
+        const character = document.createElement("div");
+        character.classList.add("character");
+        character.innerHTML = `
+        <p class="name"><b>${i.name}</b></p>
+        <p class="image"><img src="${i.image}" alt="Figura completa de ${i.name}"></p>
+        <p class="description"><b>${i.desc}</b></p>
+        `;
+        list.appendChild(character);
+    });
+};
+
+function busqueda() {
+    searchBar = document.getElementById("searchBar");
+    document.getElementById("characters").innerHTML = "";
+
+    fetch(
+        `https://dragonball-api.com/api/characters?name=${lowing(
+            searchBar.value
+        )}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                "characters"
+            ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            setTimeout(() => {
+                document.getElementById("characters").innerHTML = "";
+                const characters = getCharacters(data);
+                insertCharacters(characters);
+            }, 5000);
+        });
+}
+
+const getInitialCharacters = () => {
+    fetch(`https://dragonball-api.com/api/characters?page=1&limit=10`)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                "characters"
+            ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            prevPageLink = data.links.previous;
+            nextPageLink = data.links.next;
+            setTimeout(() => {
+                document.getElementById("characters").innerHTML = "";
+                const characters = getCharacters(data.items);
+                insertCharacters(characters);
+                subSection.style.display = "";
+                if (prevPageLink === "") {
+                    prevBttn.disabled = true;
+                } else {
+                    prevBttn.disabled = false;
+                }
+
+                if (nextPageLink === "") {
+                    nxtBttn.disabled = true;
+                } else {
+                    nxtBttn.disabled = false;
+                }
+            }, 5000);
+        })
+        .catch((error) => {
+            console.error(`ERROR, ${error}`);
+        });
+};
+
+function getResponseAPI(p) {
+    console.log(p)
+    let url = ""
+    if (p == "prev") {
+        url = prevPageLink
+    } else if (p == "next") {
+        url = nextPageLink
+    } else {
+        console.error("Parametro no válido pasado")
+        alert("Parametro no válido pasado")
+    }
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                "characters"
+            ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            prevPageLink = data.links.previous;
+            nextPageLink = data.links.next;
+            setTimeout(() => {
+                document.getElementById("characters").innerHTML = "";
+                const characters = getCharacters(data.items);
+                insertCharacters(characters);
+                subSection.style.display = "";
+                if (prevPageLink === "") {
+                    prevBttn.disabled = true;
+                } else {
+                    prevBttn.disabled = false;
+                }
+
+                if (nextPageLink === "") {
+                    nxtBttn.disabled = true;
+                } else {
+                    nxtBttn.disabled = false;
+                }
+            }, 5000);
+        })
+        .catch((error) => {
+            console.error(`ERROR, ${error}`);
+        });
+}
+
+const subSection = document.getElementById("searchSection");
+subSection.style.display = "none";
+getInitialCharacters();
+
+//Triggers
+const button = document.getElementById("bttnSearch");
+const prevBttn = document.getElementById("prevPage");
+const nxtBttn = document.getElementById("nextPage");
+button.addEventListener("click", busqueda);
+prevBttn.addEventListener("click", getResponseAPI("prev"));
+nxtBttn.addEventListener("click", getResponseAPI("next"));
+ */
