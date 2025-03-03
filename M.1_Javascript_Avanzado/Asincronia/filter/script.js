30bdad7de734b7381b9c82a1f11896146a2abef9
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-03-03 13:10:49 +0100

    Sintaxis disabled buttons mejorada

--- M.1_Javascript_Avanzado/Asincronia/filter/script.js
+++ M.1_Javascript_Avanzado/Asincronia/filter/script.js
@@ -1,31 +1,9 @@
 // Busqueda
+let prevPageLink = "";
 let nextPageLink = "";
 
 function lowing(txt) {
     return String(txt).toLowerCase();
-}
-
-function busqueda() {
-    searchBar = document.getElementById("searchBar");
-    document.getElementById("characters").innerHTML = "";
-
-    fetch(
-        `https://dragonball-api.com/api/characters?name=${lowing(
-            searchBar.value
-        )}`
-    )
-        .then((response) => response.json())
-        .then((data) => {
-            document.getElementById(
-                "characters"
-            ).innerHTML = `<p id="loading">Preparando página...</p>`;
-            setTimeout(() => {
-                document.getElementById("characters").innerHTML = "";
-                const characters = getCharacters(data);
-                console.log(characters);
-                insertCharacters(characters);
-            }, 5000);
-        });
 }
 
 const getCharacters = (p) => {
@@ -50,40 +28,89 @@ const insertCharacters = (p) => {
     });
 };
 
-const subSection = document.getElementById("searchSection");
-subSection.style.display = "none";
+function busqueda() {
+    searchBar = document.getElementById("searchBar");
+    document.getElementById("characters").innerHTML = "";
 
-fetch(`https://dragonball-api.com/api/characters`)
-    .then((response) => response.json())
-    .then((data) => {
-        document.getElementById(
-            "characters"
-        ).innerHTML = `<p id="loading">Preparando página...</p>`;
-        nextPageLink = data.links.next;
-        setTimeout(() => {
-            document.getElementById("characters").innerHTML = "";
-            const characters = getCharacters(data.items);
-            insertCharacters(characters);
-            subSection.style.display = "";
-        }, 5000);
-    })
-    .catch((error) => {
-        console.error(`ERROR, ${error}`);
-    });
-
-const nxtPage = () => {
-    fetch(`${nextPageLink}`)
+    fetch(
+        `https://dragonball-api.com/api/characters?name=${lowing(
+            searchBar.value
+        )}`
+    )
         .then((response) => response.json())
         .then((data) => {
             document.getElementById(
                 "characters"
             ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            setTimeout(() => {
+                document.getElementById("characters").innerHTML = "";
+                const characters = getCharacters(data);
+                insertCharacters(characters);
+            }, 5000);
+        });
+}
+
+const subSection = document.getElementById("searchSection");
+subSection.style.display = "none";
+
+function getResponseAPI() {
+    fetch(`https://dragonball-api.com/api/characters`)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                "characters"
+            ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            prevPageLink = data.links.previous;
             nextPageLink = data.links.next;
             setTimeout(() => {
                 document.getElementById("characters").innerHTML = "";
                 const characters = getCharacters(data.items);
                 insertCharacters(characters);
                 subSection.style.display = "";
+                if (prevPageLink === "") {
+                    prevBttn.disabled = true;
+                } else {
+                    prevBttn.disabled = false;
+                }
+
+                if (nextPageLink === "") {
+                    nxtBttn.disabled = true;
+                } else {
+                    nxtBttn.disabled = false;
+                }
+            }, 5000);
+        })
+        .catch((error) => {
+            console.error(`ERROR, ${error}`);
+        });
+}
+
+const nxtPage = () => {
+    fetch(nextPageLink)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                "characters"
+            ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            prevPageLink = data.links.previous;
+            nextPageLink = data.links.next;
+            setTimeout(() => {
+                document.getElementById("characters").innerHTML = "";
+                const characters = getCharacters(data.items);
+                insertCharacters(characters);
+                subSection.style.display = "";
+                if (prevPageLink === "") {
+                    prevBttn.disabled = true;
+                } else {
+                    prevBttn.disabled = false;
+                    // prevBttn.removeAttribute("disabled");
+                }
+                if (nextPageLink === "") {
+                    nxtBttn.disabled = true;
+                } else {
+                    nxtBttn.disabled = false;
+                    // nxtBttn.removeAttribute("disabled");
+                }
             }, 5000);
         })
         .catch((error) => {
@@ -91,8 +118,43 @@ const nxtPage = () => {
         });
 };
 
+const prevPage = () => {
+    fetch(prevPageLink)
+        .then((response) => response.json())
+        .then((data) => {
+            document.getElementById(
+                "characters"
+            ).innerHTML = `<p id="loading">Preparando página...</p>`;
+            prevPageLink = data.links.previous;
+            nextPageLink = data.links.next;
+            setTimeout(() => {
+                document.getElementById("characters").innerHTML = "";
+                const characters = getCharacters(data.items);
+                insertCharacters(characters);
+                subSection.style.display = "";
+                if (prevPageLink === "") {
+                    prevBttn.disabled = true;
+                } else {
+                    prevBttn.disabled = false;
+                }
+                if (nextPageLink === "") {
+                    nxtBttn.disabled = true;
+                } else {
+                    nxtBttn.disabled = false;
+                }
+            }, 5000);
+        })
+        .catch((error) => {
+            console.error(`ERROR, ${error}`);
+        });
+};
+
+getResponseAPI();
+
 //Triggers
 const button = document.getElementById("bttnSearch");
 button.addEventListener("click", busqueda);
+const prevBttn = document.getElementById("prevPage");
+prevBttn.addEventListener("click", prevPage);
 const nxtBttn = document.getElementById("nextPage");
 nxtBttn.addEventListener("click", nxtPage);
--- M.1_Javascript_Avanzado/Axios/README.md
+++ M.1_Javascript_Avanzado/Axios/README.md
@@ -1,3 +1,23 @@
 # Axios
 
 Se trata de una librería de JS para hacer un `fetch` de forma diferente (o más sencilla)
+
+Para poder utilizar Axios en nuestro documentos HTML, hemos de insertar su libreria en el documento con `script`:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+```
+
+```js
+const axios = require("axios");
+
+// Making a GET request
+axios
+    .get("https://api.example.com/data")
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((error) => {
+        console.error("Error fetching data:", error);
+    });
+```
