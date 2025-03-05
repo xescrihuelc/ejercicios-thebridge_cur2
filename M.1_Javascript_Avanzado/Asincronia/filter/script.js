33da397365b9a62622c0f8e0a67d4c40da44d9ba
xescrihuelc <xescrihuelc@outlook.com>
2025-03-05 01:38:48 +0100

    - Condicionales para hacer consultado en funcion de parametro 'p' pasado por getResponseAPI() añadido. - Botones 'prevBttn' y 'nxtBttn' en su desactivación con comprobaciones corregidas. - 'style.css' corregido.

--- M.1_Javascript_Avanzado/Asincronia/filter/script.js
+++ M.1_Javascript_Avanzado/Asincronia/filter/script.js
@@ -9,7 +9,7 @@ function lowing(txt) {
 const getCharacters = (p) => {
     const array = new Map();
     p.forEach((i) => {
-        array.set(i.id, { name: i.name, image: i.image, desc: i.description });
+        array.set(i.id, { id: i.id, name: i.name, image: i.image, desc: i.description });
     });
     return array;
 };
@@ -72,8 +72,7 @@ const aplyFilters = () => {
             phraseFilter = phraseFilter + 'affiliation=' + lowing(chrAffiliation.value)
         }
     };
-    console.log("URL:", encodeURI(phraseFilter))
-    return phraseFilter
+    getResponseAPI("filter", encodeURI(phraseFilter))
 }
 
 const rsetFilters = () => {
@@ -88,11 +87,12 @@ const rsetFilters = () => {
     getResponseAPI("");
 }
 
-function getResponseAPI(p) {
-    let url = "";
+function getResponseAPI(p,filter) {
+    let url = "https://dragonball-api.com/api/characters";
+
     switch (p) {
         case "":
-            url = "https://dragonball-api.com/api/characters?page=1&limit=10";
+            url = url + "?page=1&limit=10";
             break;
 
         case "prev":
@@ -104,7 +104,7 @@ function getResponseAPI(p) {
             break;
 
         case "filter":
-            filterURL = () => aplyFilters();
+            url = url + filter            
             break;
     
         default:
@@ -116,7 +116,10 @@ function getResponseAPI(p) {
     fetch(url)
         .then((response) => response.json())
         .then((data) => {
-            // Ocultar controles
+            // Ocultar controles y estilos
+            const main = document.getElementsByTagName('main')[0]
+            main.style.setProperty('justify-content', 'center')
+            
             filterBox.style.display = 'none'
             prevBttn.hidden = true;
             nxtBttn.hidden = true;
@@ -127,35 +130,49 @@ function getResponseAPI(p) {
             ).innerHTML = `<p id="loading">Preparando página...</p>`;
 
             // Variables de URLs
-            prevPageLink = data.links.previous;
-            nextPageLink = data.links.next;
+            if (p !== "filter") {
+                prevPageLink = data.links.previous;
+                nextPageLink = data.links.next;
+            }
 
             // 
             setTimeout(() => {
                 document.getElementById("characters").innerHTML = "";
-                const characters = getCharacters(data.items);
+                // data.items
+                let characters = "";
+                if (p == "filter") {
+                    characters = getCharacters(data);
+                } else {
+                    characters = getCharacters(data.items);
+                }
                 insertCharacters(characters);
                 filterBox.style.display = "";
 
                 // Condiciones que muestran/ocultan botones de prevPage o nextPage en
                 // función de si exísten URLs o no.
-                if (prevPageLink === "") {
+                if (p !== "filter") {
+                    if (prevPageLink === "" || !data.links) {
+                        prevBttn.disabled = true;
+                    } else {
+                        prevBttn.disabled = false;
+                    }
+                    
+                    if (nextPageLink === "" || !data.links) {
+                        nxtBttn.disabled = true;
+                    } else {
+                        nxtBttn.disabled = false;
+                    }
+                } else {
                     prevBttn.disabled = true;
-                } else {
-                    prevBttn.disabled = false;
-                }
-                
-                if (nextPageLink === "") {
                     nxtBttn.disabled = true;
-                } else {
-                    nxtBttn.disabled = false;
                 }
 
-                // Mostrar botones de control
+                // Mostrar botones de control y estilos
                 filterBox.style.display = ''
+                main.style.setProperty('justify-content', 'space-between')
                 prevBttn.hidden = false;
                 nxtBttn.hidden = false;
-            }, 5000);
+            }, 2000);
         })
         .catch((error) => {
             console.error(`ERROR, ${error}`);
@@ -177,5 +194,4 @@ const applyFilters = document.getElementById("applyFilters");
 prevBttn.addEventListener("click", () => getResponseAPI("prev"));
 nxtBttn.addEventListener("click", () => getResponseAPI("next"));
 restoreFilters.addEventListener("click", rsetFilters);
-// applyFilters.addEventListener("click", () => getResponseAPI("filter"));
 applyFilters.addEventListener("click", aplyFilters);
