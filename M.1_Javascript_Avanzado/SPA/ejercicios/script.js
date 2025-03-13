fb309fc9b3749e55309df4271aaba6c46f7e3985
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-03-13 13:47:30 +0100

    Estructura inicial creada

--- M.1_Javascript_Avanzado/SPA/ejercicios/script.js
+++ M.1_Javascript_Avanzado/SPA/ejercicios/script.js
@@ -2,6 +2,15 @@ fetch("https://opentdb.com/api.php?amount=10&type=multiple")
     .then((response) => response.json())
     .then((data) => {
         const questions = data.results;
-        console.log(questions);
+        questions.forEach((quest) => {
+            console.log(quest.question);
+            console.log("Correct ans:", quest.correct_answer);
+            console.log("FAIL 1:", quest.incorrect_answers[0]);
+            console.log("FAIL 2:", quest.incorrect_answers[1]);
+            console.log("FAIL 3:", quest.incorrect_answers[2]);
+        });
     })
-    .catch();
+    .catch((error) => {
+        console.error("ERROR OBTENCION DATOS API", error);
+        alert("Error al obtener datos de la API");
+    });
