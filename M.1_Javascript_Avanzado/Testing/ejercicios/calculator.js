60d53587e4068171b81084cb6cecef49b9d40ede
Joaquim Escrihuela <xescrihuelc@outlook.com>
2025-03-25 10:19:11 +0100

    Funcionalidades añadidas y correcciones estructuráles de código

--- M.1_Javascript_Avanzado/Testing/ejercicios/calculator.js
+++ M.1_Javascript_Avanzado/Testing/ejercicios/calculator.js
@@ -14,10 +14,23 @@ const divide = (a, b) => {
         return a / b;
     }
 };
+const elevPoten = (a, b) => {
+    return a ** b;
+};
+const squareRoot = (a, b) => {
+    if (a && b) {
+        return "Debe haber solo un número para calcular la raíz cuadrada";
+    } else if (b != null) {
+        return "Pon el número en el otro campo";
+    } else {
+        return Math.sqrt(a);
+    }
+};
 
 function operate() {
     // Variables de control para el switch
     let resultOperation = null;
+    const resultParagraf = document.getElementById("resultado");
 
     //
     switch (numOperation) {
@@ -40,25 +53,35 @@ function operate() {
                 );
             }
             break;
-
+        case 5:
+            resultOperation = elevPoten(Number(num1.value), Number(num2.value));
+            break;
+        case 6:
+            if (Number(num2.value) == "") {
+                resultOperation = squareRoot(Number(num1.value));
+            } else if (Number(num1.value) == "") {
+                resultOperation = squareRoot(null, Number(num2.value));
+            } else {
+                resultOperation = squareRoot(
+                    Number(num1.value),
+                    Number(num2.value)
+                );
+            }
+            break;
         default:
             console.error("ERROR | NO HAS SELECCIONADO UNA OPERACIÓN");
             alert("ERROR | NO HAS SELECCIONADO UNA OPERACIÓN");
             break;
     }
 
-    if (isNaN(resultOperation)) {
-        document.getElementById(
-            "resultado"
-        ).innerHTML = `En algúno de los campos para intruducir números no hay ningún número. INTRUDUCE EN LOS DOS CAMPOS UN NÚMERO`;
+    if ((numOperation = 6)) {
+        resultParagraf.innerHTML = `El resultado de la operación es: ${resultOperation}`;
+    } else if (isNaN(resultOperation)) {
+        resultParagraf.innerHTML = `En algúno de los campos para intruducir números no hay ningún número. INTRUDUCE EN LOS DOS CAMPOS UN NÚMERO`;
         return;
+    } else {
+        resultParagraf.innerHTML = `El resultado de la operación es: ${resultOperation}`;
     }
-
-    document.getElementById(
-        "resultado"
-    ).innerHTML = `El resultado de la operación es: ${resultOperation}`;
-
-    return resultOperation;
 }
 
 // Función para que al cargar la página,
--- M.1_Javascript_Avanzado/Testing/ejercicios/test.js
+++ M.1_Javascript_Avanzado/Testing/ejercicios/test.js
@@ -26,3 +26,20 @@ console.log(
         "divide(5, 0) debería manejar la división por cero:"
     )
 );
+console.log(assertEquals(elevPoten(2, 3), 8, "elevPoten(2, 3) debería ser 8:"));
+console.log(assertEquals(elevPoten(3, 0), 1, "elevPoten(3, 0) debería ser 1:"));
+console.log(assertEquals(squareRoot(25), 5, "squareRoot(25,) debería ser 5:"));
+console.log(
+    assertEquals(
+        squareRoot(3, 4),
+        "Debe haber solo un número para calcular la raíz cuadrada",
+        "squareRoot(3, 4) debería ser indicar que no ha de tener numeros en los dos campos para realizar la operación:"
+    )
+);
+console.log(
+    assertEquals(
+        squareRoot(null, 4),
+        "Pon el número en el otro campo",
+        "squareRoot(, 4) debería decír que ponga el número en el otro campo:"
+    )
+);
